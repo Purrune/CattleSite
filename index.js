794ad34ef2a0b4cc41f607b1f31c59365bf71ee3
Purrune <Dkenny199@gmail.com>
2023-06-12 18:14:01 -0400

    update

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function newCattle(){
     let Ncattle = document.getElementById("newCattle").value;
     alert(Ncattle);
+    const textNode = 
+        document.createTextNode(Ncattle);
+        document.body.appendChild(textNode);
 }
+function cattle(){
+    let textNode = 
+        document.createTextNode(Ncattle);
+        document.body.appendChild(textNode);
+}
+
